3c33f89beb6cdf8a379d3d6df85a8f786a056620
Adarsh Iyer <adarsh.iyer17@gmail.com>
2018-04-20 18:56:20 -0700

    Latest game.js changes

--- src/client/js/game.js
+++ src/client/js/game.js
@@ -1,8 +1,8 @@
-
 var playerX = window.innerWidth / 2;
 var playerY = window.innerHeight / 2;
 var playerRadius = 100;
-var moveVelocity = 5;
+
+var playerSpeed = 2;
 
 function setup() {
   var canvas = createCanvas(window.innerWidth, window.innerHeight);
@@ -11,19 +11,39 @@ function setup() {
   
 }
 
+var stepX = 0;
+var stepY = 0;
+
 function draw() {
-  var stepX;
-  var stepY;
+  // push();
+  // translate(window.innerWidth / 2, window.innerHeight / 2);
+  var shouldIncrement = false;
+  var stepX = 0;
+  var stepY = 0;
+
+  var mouseXC = mouseX - window.innerWidth / 2;
+  var mouseYC = mouseY - window.innerHeight / 2;
+
+  var theta = Math.atan2(mouseYC, mouseXC);
+
+  var move = Math.sqrt(Math.pow(mouseXC, 2) + Math.pow(mouseYC, 2)) > playerRadius;
+
+  if(move) {
+    stepX += Math.cos(theta) * playerSpeed;
+    stepY += Math.sin(theta) * playerSpeed;
+  }
+
+  console.log(mouseXC + ", " + mouseYC);
 
   clear();
   ellipse(playerX, playerY, 2*playerRadius, 2*playerRadius);
-
   push();
-  translate(stepX, stepY);
-  ellipse(20, 20, 30, 30);
-  ellipse(40, 40, 30, 30);
-  ellipse(60, 60, 30, 30);
-  ellipse(80, 80, 30, 30);
+  if(move)
+    translate(stepX, stepY);
+  ellipse(20, 20, 300, 300);
+  ellipse(40, 40, 300, 300);
+  ellipse(60, 60, 300, 300);
+  ellipse(80, 80, 300, 300);
   pop();
 }
 
