cd3edc8c3f2eaf7b0a24cdc7d38c2435db85f906
Eric Qian <eric@enumc.com>
2019-01-03 01:26:20 -0800

    FIX prepush hook doc update
    [updatedDoc]

--- prepush.sh
+++ prepush.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Hail the glorieous BASH
-if git ls-files docs --other --directory --exclude-standard | grep -q docs
+if git add docs/ -A -n | grep -q docs
 then
-    git add .
+    git add docs/ -A    # -n flag is dry run
     git commit --amend -m "$(git log --format=%B -n1)" -m "[updatedDoc]" --no-verify
 else
     echo "No Doc changes detected in current work tree. Skipping doc update!"
